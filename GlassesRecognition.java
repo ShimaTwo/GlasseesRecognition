b1faf70e8ac84c6eea565885317cd28c11e1cefa
ShimaTwo <itfs1311368@icloud.com>
2019-11-23 10:04:27 +0900

    tmp

---
+++ GlassesRecognition.java
@@ -0,0 +1,7 @@
+
+public class GlassesRecognition {
+    public static void main(String args[]) {
+        // 画像読み込み
+        RGBImage rgbImage[] = new RGBImage[2];
+    }
+}
---
+++ RGBImage.java
@@ -0,0 +1,12 @@
+public class RGBImage {
+    private double[][] red;
+    private double[][] green;
+    private double[][] blue;
+
+    private
+    RGBImage(){
+        
+    }
+
+
+}
